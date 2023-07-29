edec4602f63f18ec4b4bcf870052fe31bd7535c2
Harry Yep <github@hode.co.uk>
2023-07-29 16:03:42 +0800

    tensorflow

--- tensorflow/linear-predict.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import tensorflow as tf
-
-
-x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data * 0.1 + 0.3
--- tensorflow/tutorial/TensorFlow-Course/1.basic.py
+++ tensorflow/tutorial/TensorFlow-Course/1.basic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
+
 # Tensor
 print("\n --- Tensor --- \n")
 
@@ -12,7 +13,9 @@ print("\n")
 
 tensor_2 = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9])
 print("Tensor:", tensor_2)
-print("Rank:", tf.rank(tensor_2).numpy())
+print(
+    "Rank:", tf.rank(tensor_2).numpy()
+)  # NOTE: We use .numpy() to transform tf.tensor to numpy
 
 
 # Tensor operations
@@ -21,7 +24,9 @@ print("\n --- Tensor operations --- \n")
 x = tf.constant([[1, 1], [1, 1]])
 y = tf.constant([[2, 4], [6, 8]])
 
+# Add two tensors
 print(tf.add(x, y), "\n")
+# Add two tensors
 print(tf.matmul(x, y), "\n")
 
 
@@ -29,6 +34,7 @@ print(tf.matmul(x, y), "\n")
 print("\n --- Multi-dimensional tensors --- \n")
 
 tensor_3 = tf.ones(shape=[1, 2, 3], dtype=tf.float32)
+
 print("Tensor:", tensor_3)
 print("Tensor Rank: ", tf.rank(tensor_3).numpy())
 print("Shape: ", tensor_3.shape)
@@ -44,9 +50,13 @@ print("\n --- Indexing --- \n")
 
 tensor_4 = tf.constant([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
+# All elements
 print(tensor_4[:].numpy())
+# All elements of the first row
 print(tensor_4[0, :].numpy())
+# First row and last column
 print(tensor_4[0, -1].numpy())
+# From second row to last and from third column to last
 print(tensor_4[1:, 2:].numpy)
 
 
@@ -57,6 +67,7 @@ tensor_5 = tf.constant([1, 2, 3, 4], dtype=tf.float32)
 print("Tensor:", tensor_5)
 print("Tensor type:", tensor_5.dtype)
 
+# Casting to change dtype
 casted_tensor_5 = tf.cast(tensor_5, dtype=tf.int32)
 print("Casted tensor:", casted_tensor_5)
 print("Casted tensor type:", casted_tensor_5.dtype)
